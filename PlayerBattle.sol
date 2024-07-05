b654c7cbbd3af5961fe3e7e6306118f9727648f5
Tranquil-Flow <66773372+Tranquil-Flow@users.noreply.github.com>
2024-07-05 17:09:59 +0200

    implement simple battling system

--- PlayerBattle.sol
+++ PlayerBattle.sol
@@ -8,43 +8,44 @@ contract PlayerBattle {
 
     struct Battles {
         uint battleID;
-        address monsterA;
-        address monsterB;
-        uint winner;    // 0 = undeclared, 1 = monsterA, 2 = monsterB
+        uint monsterA_ID;
+        uint monsterB_ID;
+        uint winner;    // 0 = undeclared, 1 = monsterA_ID, 2 = monsterB_ID
     }
 
     Battles[] public battles;
 
     struct BattleOffers {
-        address monsterToBattle;
+        uint monsterToBattle;
         uint deadline;
         bool offerAccepted;
     }
 
-    mapping(address => BattleOffers[]) public battleOfferList;
+    mapping(uint => BattleOffers[]) public battleOfferList;
 
-    event BattleOfferOpened(address monsterInitiatingBattle, address monsterReceivingBattleOffer, uint battleDeadline);
-    event BattleOfferClosed(address monsterInitiatingBattle, address monsterReceivingBattleOffer);
-    event BattleStarted(address monsterA, address monsterB, uint battleStartTime);
-    event BattleFinished(address monsterA, address monsterB, uint battleFinishTime, address winner);
+    event BattleOfferOpened(uint monsterInitiatingBattle, uint monsterReceivingBattleOffer, uint battleDeadline);
+    event BattleOfferClosed(uint monsterInitiatingBattle, uint monsterReceivingBattleOffer);
+    event BattleStarted(uint monsterA_ID, uint monsterB_ID, uint battleStartTime);
+    event BattleFinished(uint monsterA_ID, uint monsterB_ID, uint battleFinishTime, uint winner);
 
     error NoBattleOffer;
     error BattleOfferExpired;
     error FightAlreadyAccepted;
 
-    function openBattleOffer(address _monsterToBattle, uint _offerLength) external {
+    function openBattleOffer(uint _monsterToBattle, uint _offerLength) external {
         uint deadline = block.timestamp + _offerLength;
         battleOffers[msg.sender] = BattleOffers(_monsterToBattle, deadline, false);
         emit BattleOfferOpened(msg.sender, _monsterToBattle, deadline); 
     }
 
-    function closeBattleOffer(address _monsterToNotBattle) external {
+    function closeBattleOffer(uint _monsterToNotBattle) external {
         battleOffers[msg.sender] = BattleOffers(_monsterToNotBattle, 0, false);
         emit BattleOfferClosed(msg.sender, _monsterToNotBattle);
     }
 
-    function acceptBattleOffer(address monsterFighting) external {
-        address monsterAcceptingOffer = msg.sender;
+    function acceptBattleOffer(uint monsterAcceptingOffer, uint monsterFighting) external {
+        // Check that the owner of the monster is the one accepting offer
+
         if (battleOfferList[monsterFighting].monsterToBattle != monsterAcceptingOffer) {
             revert NoBattleOffer();
         }
@@ -61,15 +62,47 @@ contract PlayerBattle {
         emit BattleStarted(monsterFighting, monsterAcceptingOffer, block.timestamp);
     }
 
-    function monsterBattle(address _monsterA, address _monsterB) public {
-        // load monsterA's interface
-        // load monsterB's interface
+    function monsterBattle(uint monsterA_ID, uint monsterB_ID) public {
+        IMonster monsterA = IMonster(address(monsterAContract));
+        IMonster monsterB = IMonster(address(monsterBContract));
 
+        (uint healthA, uint attackA, uint defenseA, uint speedA) = monsterA.getMonsterAttributes(monsterA_ID);
+        (uint healthB, uint attackB, uint defenseB, uint speedB) = monsterB.getMonsterAttributes(monsterB_ID);
+
+        uint currentHealthA = healthA;
+        uint currentHealthB = healthB;
+
+        while (currentHealthA > 0 && currentHealthB > 0) {
+            if (speedA >= speedB) {
+                // Monster A attacks first
+                uint damage = attackA > defenseB ? attackA - defenseB : 0;
+                currentHealthB = currentHealthB > damage ? currentHealthB - damage : 0;
+
+                if (currentHealthB > 0) {
+                    // Monster B counterattacks
+                    damage = attackB > defenseA ? attackB - defenseA : 0;
+                    currentHealthA = currentHealthA > damage ? currentHealthA - damage : 0;
+                }
+            } else {
+                // Monster B attacks first
+                uint damage = attackB > defenseA ? attackB - defenseA : 0;
+                currentHealthA = currentHealthA > damage ? currentHealthA - damage : 0;
+
+                if (currentHealthA > 0) {
+                    // Monster A counterattacks
+                    damage = attackA > defenseB ? attackA - defenseB : 0;
+                    currentHealthB = currentHealthB > damage ? currentHealthB - damage : 0;
+                }
+            }
+        }
+
+        uint winner = currentHealthA > 0 ? 1 : 2;
+        finishFight(monsterA_ID, monsterB_ID, winner);
     }
 
-    function finishFight(address _monsterA, address _monsterB, address _winner) public {
-        // push info to battles
-        emit BattleFinished(_monsterA, _monsterB, block.timestamp, _winner);
+    function finishFight(uint _monsterA_ID, uint _monsterB_ID, uint _winner) public {
+        battles.push(Battles(totalBattles, monsterA_ID, monsterB_ID, winner));
+        emit BattleFinished(_monsterA_ID, _monsterB_ID, block.timestamp, _winner);
     }
 
 }
