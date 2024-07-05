b97a71a9e5261029ae5ae14ce3f7c67ade50d281
Tranquil-Flow <66773372+Tranquil-Flow@users.noreply.github.com>
2024-07-05 15:38:18 +0200

    change SpawningStone to immutable, add view funcs

--- SpawningStone.sol
+++ SpawningStone.sol
@@ -1,12 +1,11 @@
 pragma solidity 0.8.25;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 // TO-DO: Only msg.sender can mint? (AA impllications?)
 
-contract SpawningStone is Initializable, ERC721Upgradeable, OwnableUpgradeable {
+contract SpawningStone is ERC721, Ownable {
     mapping(uint256 => string) private _tokenURIs;
     uint256 private _currentTokenId;
 
@@ -15,10 +14,13 @@ contract SpawningStone is Initializable, ERC721Upgradeable, OwnableUpgradeable {
         bool value;
     }
 
-    mapping(uint => mapping(string => bool)) public traits;   // tokenId => (traitName => traitValue)
+    string[] public traitList;
+    mapping(uint => mapping(string => bool)) public spawningStoneTraits;   // tokenId => (traitName => traitValue)
 
     error TokenDoesNotExist(uint256 tokenId);
+    error TraitNotValid(string traitName);
 
+    event GlobalTraitAdded(string traitName);
     event TraitAdded(uint indexed tokenID, string traitName, bool value);
 
     function initialize() initializer public {
@@ -45,10 +47,45 @@ contract SpawningStone is Initializable, ERC721Upgradeable, OwnableUpgradeable {
         return _tokenURIs[tokenId];
     }
 
-    function addTrait(uint256 tokenId, string memory traitName, bool value) external onlyOwner {
+    function getTrait(uint256 tokenId, string memory traitName) public view returns (bool) {
+        if (!_exists(tokenId)) {
+            revert TokenDoesNotExist(tokenId);
+        }
+        return tokenTraits[tokenId][traitName];
+    }
+
+    function getTraits(uint256 tokenId) public view returns (string[] memory) {
         if (!_exists(tokenId)) {revert TokenDoesNotExist(tokenId);}
-        tokenTraits[tokenId][traitName] = value;
+
+        uint256 count;
+        for (uint256 i = 0; i < traitList.length; i++) {
+            if (tokenTraits[tokenId][traitList[i]]) {
+                count++;
+            }
+        }
+
+        string[] memory traits = new string[](count);
+        uint256 index;
+        for (uint256 i = 0; i < traitList.length; i++) {
+            if (tokenTraits[tokenId][traitList[i]]) {
+                traits[index] = traitList[i];
+                index++;
+            }
+        }
+
+        return traits;
+    }
+
+    function addTraitToSpawningStone(uint256 tokenId, string memory traitName, bool value) external onlyOwner {
+        if (!_exists(tokenId)) {revert TokenDoesNotExist(tokenId);}
+        if (!isValidTrait(traitName)) {revert TraitNotValid(traitName);}
+        spawningStoneTraits[tokenId][traitName] = value;
         emit TraitAdded(tokenId, traitName, value);
+    }
+
+    function addGlobalTrait(string memory traitName) external onlyOwner {
+        traitList.push(traitName);
+        emit GlobalTraitAdded(traitName);
     }
 
 }
